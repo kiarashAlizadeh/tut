f681a706e74eeab12c26b1aa71bd6b616542466b
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2023-12-30 12:18:14 +0330

    images format converted to webp

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,23 +10,23 @@ import contactUs from "./icons/navbar/contactUs.svg"
 import aboutUs from "./icons/navbar/aboutUs.svg"
 
 // images
-import fullLogo from "./images/fullLogo.svg"
-import Logo from "./images/Logo.svg"
+import fullLogo from "./images/fullLogo.webp"
+import Logo from "./images/Logo.webp"
 import bgCSharp from "./images/courses/bg-cSharp.svg"
-import Header1 from "./images/Header1.png"
-import notFound from "./images/notFound.png"
+import Header1 from "./images/Header1.webp"
+import notFound from "./images/notFound.webp"
 
 // courses
 import cSharp from "./images/courses/cSharp.svg"
-import cSharpCourse from "./images/courses/cSharpCourse.png"
+import cSharpCourse from "./images/courses/cSharpCourse.webp"
 import pythonCourse from "./images/courses/pythonCourse.webp"
-import frontEndCourse from "./images/courses/frontEndCourse.jpg"
+import frontEndCourse from "./images/courses/frontEndCourse.webp"
 // import bgMainCSharp from "./images/courses/bgMainCSharp.svg"
-import bgMainCSharp from "./images/courses/Group6.png"
+import bgMainCSharp from "./images/courses/Group6.webp"
 import python from "./images/courses/python.svg"
 import bgMainPython from "./images/courses/bgMainPython2.svg"
 import webLogos from "./images/courses/webLogos.svg"
-import bgMainWeb from "./images/courses/Group7.png"
+import bgMainWeb from "./images/courses/Group7.webp"
 
 export {
   fullLogo,
