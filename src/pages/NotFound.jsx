cf20065350387dc32a8ef391bc6a4dda92297fa5
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2023-12-30 10:17:38 +0330

    notFound page style changed

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,21 +6,22 @@ function NotFound() {
   return (
     <>
       <Navbar />
-      <div className="mx-auto mt-5 flex max-h-dvh max-w-[800px] flex-col items-center sm:h-auto select-none">
-        <h1 className="mb-5 text-2xl font-bold text-[#095373] md:text-3xl">
+      <div className="mx-auto mt-5 flex max-h-dvh max-w-[800px] select-none flex-col items-center sm:h-auto">
+        <h1 className="mb-1 text-2xl font-bold text-[#095373] md:text-3xl">
           !Error
         </h1>
-        <div className="mb-5 text-lg font-semibold text-[#095373] md:text-xl">
-          صفحه مورد نظر شما پیدا نشد!
-        </div>
+
         <img
           src={notFound}
           alt="notFound"
-          className="max-h-[500px] select-none"
+          className="md:max-h-[500px] max-h-[240px] select-none"
           draggable="false"
         />
+        <div className="mb-5 text-lg font-semibold text-[#095373] md:text-xl">
+          صفحه مورد نظر شما پیدا نشد!
+        </div>
         <button
-          className="rounded-lg bg-[#0062d1] px-4 py-2 font-semibold text-white md:text-xl hover:bg-opacity-70"
+          className="rounded-lg bg-[#0062d1] px-4 py-2 font-semibold text-white hover:bg-opacity-70 md:text-xl"
           onClick={() => (window.location.href = "/")}
         >
           بازگشت به صفحه اصلی
